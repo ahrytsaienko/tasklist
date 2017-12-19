d4f0b844c233843034cb330522cd51d3e094c853
temsan89 <temsan89@gmail.com>
2017-12-19 21:42:22 +0200

    task list, add, delete, filter

---
+++ app.js
@@ -0,0 +1,83 @@
+// Define our UI Variables
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+    //Add task event
+    form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear tasks
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
+}
+
+function addTask(e) {
+
+    if (taskInput.value === '') {
+        alert('add a task');
+    }
+
+    // Create li element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //Append the link to li
+    li.appendChild(link);
+
+    //Append li to ul
+    taskList.appendChild(li);
+
+
+    //Clear input
+    //taskInput.value = '';
+
+    e.preventDefault();
+}
+
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm("Are u sure?")) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+function clearTasks() {
+    // taskList.innerHTML = '';
+
+    //Faster
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach
+        (function (task) {
+            const item = task.firstChild.textContent;
+            if (item.toLocaleLowerCase().indexOf(text) != -1) {
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none'
+            }
+        });
+}
